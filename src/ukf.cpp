9eeb5d00ed4837246d936cd43ca81b2e18f5a820
Steve <stevefrazee123@gmail.com>
2018-10-09 21:45:49 -0400

    normalize delta_t

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -117,7 +117,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 	}
 	
 	// calculate time change
-	double delta_t = meas_package.timestamp_ - time_us_;
+	double delta_t = (meas_package.timestamp_ - time_us_) / 1000000.0;
 
 	// call prediction
 	Prediction(delta_t);
